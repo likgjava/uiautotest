e57878a41e83f921733237d6225012be806f99a5
likaige <likaige@100tal.com>
2017-12-22 18:23:39 +0800

    testng

---
+++ src/main/java/com/likg/uiautotest/testng/TestngGroupsOfGroups.java
@@ -0,0 +1,30 @@
+package com.likg.uiautotest.testng;
+
+import org.testng.annotations.Test;
+
+/**
+ * 组测试
+ *
+ * @author likg
+ */
+public class TestngGroupsOfGroups {
+    @Test(groups = {"android.4"})
+    public void f1() {
+        System.err.println("groups=android.4 f1...");
+    }
+
+    @Test(groups = {"android.5"})
+    public void f2() {
+        System.err.println("groups=android.5 f2...");
+    }
+
+    @Test(groups = {"android.5.2"})
+    public void f3() {
+        System.err.println("groups=android.5.2 f3...");
+    }
+
+    @Test(groups = {"android.6"})
+    public void f4() {
+        System.err.println("groups=android.6 f4...");
+    }
+}
