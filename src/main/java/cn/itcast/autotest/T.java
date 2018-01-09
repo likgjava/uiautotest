1e0642bdf113a4ce8f4f8bb9a01935acbdc04dcc
likaige <likaige@100tal.com>
2018-01-09 17:31:02 +0800

    jenkins测试

--- src/main/java/cn/itcast/autotest/T.java
+++ src/main/java/cn/itcast/autotest/T.java
@@ -2,7 +2,9 @@ package cn.itcast.autotest;
 
 import org.testng.annotations.Test;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class T {
 
@@ -17,6 +19,13 @@ public class T {
 
         int i = cal.get(Calendar.DAY_OF_WEEK);
         System.out.println(i);
+
+    }
+
+    @Test
+    public void printTime() {
+        String format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        System.out.println("time==" + format);
 
     }
 
